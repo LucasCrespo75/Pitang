b37b5bdcea67e2b5046b4531fa3e91f07f70feb5
LucasCrespo75 <65462062+LucasCrespo75@users.noreply.github.com>
2021-07-13 07:52:17 -0300

    Metodo alterar usuario adicionado, juntamente com os geteres e seteres

--- PitangProject/src/main/java/controle/UsuarioBean.java
+++ PitangProject/src/main/java/controle/UsuarioBean.java
@@ -85,6 +85,84 @@ public class UsuarioBean {
 
 		}
 }
+public void alterarUsuario() {
+		
+		Usuario usuarioNovo = new Usuario();
+		
+		usuarioNovo.setNome(this.getNome());
+		usuarioNovo.setEmail(this.getEmail());
+		usuarioNovo.setSenha(this.getSenha());
+		usuarioNovo.setTelefone(this.getTelefone());
+		
+		boolean achou = false;
+		
+		this.listarTodos = this.dao.listarTodos();
+		for(Usuario usuarioPesquisa : listarTodos) {
+			if()
+		}
+	}
+	
+	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Usuario> getListarTodos() {
+		return listarTodos;
+	}
+
+	public void setListarTodos(List<Usuario> listarTodos) {
+		this.listarTodos = listarTodos;
+	}
+
+	public UsuarioDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(UsuarioDAO dao) {
+		this.dao = dao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
+
+
+	
 
 }
 	
